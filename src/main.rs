cd7dbc77d31dbe86b808571c552eda5a290a11d7
CalC <maheshwarivinayak90@gmail.com>
2025-03-09 20:49:45 +0530

    1st commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[derive(Debug)]
+struct Task {
+    id: u64,
+    name: String,
+    isCompleted: bool,
+}
+
+impl Task {
+    fn new(name: String) -> Self {
+        Task {
+            id,
+            name,
+            isCompleted: false,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let task1 = Task::new("Task 1".to_string());
+    println!("{:?}", task1);
 }
