7c19cdacfd48bc7220c78f07284540df861b6628
CalC <maheshwarivinayak90@gmail.com>
2025-03-09 20:21:34 +0530

    initial commit

---
+++ target/debug/deps/rustodo-79638fda8c6cde33.d
@@ -0,0 +1,5 @@
+/home/calc/Documents/rustodo/target/debug/deps/librustodo-79638fda8c6cde33.rmeta: src/main.rs
+
+/home/calc/Documents/rustodo/target/debug/deps/rustodo-79638fda8c6cde33.d: src/main.rs
+
+src/main.rs:
