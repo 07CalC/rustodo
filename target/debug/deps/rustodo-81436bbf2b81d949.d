7f00d4ed13f048d140b87a33d09055761258c427
CalC <maheshwarivinayak90@gmail.com>
2025-03-09 20:50:45 +0530

    fixed

--- target/debug/deps/rustodo-81436bbf2b81d949.d
+++
@@ -1,5 +0,0 @@
-/home/calc/Documents/rustodo/target/debug/deps/librustodo-81436bbf2b81d949.rmeta: src/main.rs
-
-/home/calc/Documents/rustodo/target/debug/deps/rustodo-81436bbf2b81d949.d: src/main.rs
-
-src/main.rs:
